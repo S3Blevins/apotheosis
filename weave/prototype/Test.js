331a08ed304d03eebecbf19c1945be7b31b7a3c2
damon_estrada <damon.estrada@student.nmt.edu>
2019-10-06 21:51:38 -0600

    changed output.

--- weave/prototype/Test.js
+++ weave/prototype/Test.js
@@ -3,16 +3,15 @@ var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 var auth_url = 'https://accounts.spotify.com/authorize';
 var response_type = 'code';
+var scopes = 'user-read-private';// the scopes we are asking the user to agree to.
 var state = generateRandomString(16);
-
-var scopes = 'user-read-private&';// the scopes we are asking the user to agree to.
 
 
 function authorizeUser() {
     var url = auth_url + '?client_id=' + client_id + '&response_type=' +
         response_type + '&redirect_uri=' + redirect_uri + '&scope=' +
         scopes + '&state=' + state;
-    document.write(url);
+    console.log(url);
 }
 
 /**
@@ -21,7 +20,7 @@ function authorizeUser() {
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-var generateRandomString = function(length) {
+function generateRandomString(length) {
     var text = '';
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -29,7 +28,7 @@ var generateRandomString = function(length) {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
-};
+}
 
 /* Example auth url: https://accounts.spotify.com/authorize?client_id=4429c460396649ee99abf2b6242fadde
 &response_type=code&redirect_uri=http://localhost:8084/WebApplication2/&scope=user-read-private&state=34fFs29kd09 */
